0c6627e8f060c89681a94b5dfabef6c206ed4c0b
Tomek Rogalski <tomasz@madebyrogal.com>
2019-02-01 09:46:37 +0100

    MOD Layout

--- src/react/Layout.js
+++ src/react/Layout.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default class Layout extends React.Component{
 	constructor(props){
-		super(props)		
+		super(props)
 	}
+	
 	componentWillMount(){
 		console.log("Component mounting")
 	}
@@ -13,18 +14,26 @@ export default class Layout extends React.Component{
 			<div>
 				<hr/>
 				<h1>React TO-DO</h1>
-				<ul>{this.props.todos.map((todo,key)=>{
-					return <li key={key} onClick={ (event) => {
-									  this.props.markComplete(todo)
-									}
-								} className={["list-item", todo.done === true? "done-true":"done-false"].join(" ")}>{todo.text}
-						    </li>})
-				}</ul>
-				<button id="click" onClick={ () => {
-						this.props.newItem("Alter triggered from React but Fired from AngularJS")
-					  }
-					}>Click to make Angular Alert!!</button>
+				<ul>
+                    { this.props.todos.map((todo,key) => {
+                        return (
+                            <li
+                                key={key}
+                                onClick={ () => { this.props.markComplete(todo)}}
+                                className={["list-item", todo.done === true? "done-true":"done-false"].join(" ")}
+                            >
+                                {todo.text}
+                            </li>
+                        )
+                    })}
+				</ul>
+				<button
+                    id="click"
+                    onClick={ () => { this.props.newItem("Alter triggered from React but Fired from AngularJS") }}
+                >
+                    Click to make Angular Alert!!
+				</button>
 			</div>
-		);
+		)
 	}
 }
